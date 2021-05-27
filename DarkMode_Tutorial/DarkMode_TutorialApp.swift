9c433083888f03ef79625d9379b586ce31bbc034
Seokhyun Kim <wjapp72@gmail.com>
2021-05-27 14:38:19 -0700

    Initial Commit

---
+++ DarkMode_Tutorial/DarkMode_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkMode_TutorialApp.swift
+//  DarkMode_Tutorial
+//
+//  Created by Seokhyun Kim on 2021-05-27.
+//
+
+import SwiftUI
+
+@main
+struct DarkMode_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
